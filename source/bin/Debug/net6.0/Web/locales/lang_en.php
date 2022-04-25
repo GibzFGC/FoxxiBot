1cd279368d73ee0ccaa509b80d1bae638bc81f9a
ZWEEK <none@none.com>
2022-04-25 18:28:01 +0200

    First Translations

---
+++ source/bin/Debug/net6.0/Web/locales/lang_en.php
@@ -0,0 +1,71 @@
+<?php 
+define("_ACTIVE", "Down for Maintenance");
+
+//TOP
+define("_TITLE", "FoxxiBot | Dashboard");
+define("_HOME", "Home");
+define("_NOTIFICATION", "Notifications");
+define("_NOTIFICATION_ALL", "See All Notifications");
+
+//SIDEBAR
+define("_BOTNAME", "FoxxiBot");
+define("_LOADING", "Loading...");
+define("_DASHBOARD", "Dashboard");
+define("_MANAGEMENT", "Management");
+
+define("_TOURNAMENT_FEAT", "TOURNAMENT FEATURES");
+define("_TOURNAMENT_SCORE", "Scoreboard");
+define("_TOURNAMENT_TOP", "Top 8");
+define("_TOURNAMENT_PLAYER", "Player Management");
+
+define("_TWITCH_FEAT", "TWITCH FEATURES");
+define("_TWITCH_CMDS", "Twitch Commands");
+define("_TWITCH_ADD_CMD", "Add a Command");
+
+define("_AUDIO", "Audio / Sounds");
+define("_AUDIO_ADD", "Add an Audio / Sound");
+
+define("_GIVEAWAY", "Giveaway");
+
+define("_MODERATION", "Moderation");
+define("_MODERATION_WL_BL", "Whitelist / Blacklist");
+define("_MODERATION_SETTINGS", "Moderation Settings");
+
+define("_TICKER", "Ticker");
+define("_TICKER_ADD", "Add a Tick");
+
+define("_TIMERS", "Timers");
+define("_TIMERS_ADD", "Add a Timer");
+
+define("_TWITCH_SETTINGS", "Twitch Settings");
+
+define("_DISCORD_FEAT", "DISCORD FEATURES");
+define("_DISCORD_CMDS", "Discord Commands");
+define("_DISCORD_ADD_CMD", "Add a Command");
+
+define("_PROMO", "Promo / Streamers");
+
+define("_DISCORD_SETTINGS", "Discord Settings");
+
+define("_OTHER_SERVICE", "OTHER SERVICES");
+
+define("_TWITTER", "Twitter");
+define("_TWITTER_GAME_STATUS", "Add a Game Live Status");
+define("_TWITTER_STATUS_LIST", "Twitter Status List");
+define("_TWITTER_SETTINGS", "Twitter Settings");
+
+define("_GLOBAL_FEAT", "GLOBAL FEATURES");
+
+define("_POINTS", "Points");
+define("_POINT_RANK", "Point Rankings");
+define("_POINT_REDEEM", "Point Redeems");
+
+define("_QUOTES", "Quotes");
+define("_QUOTES_ADD", "Add a Quote");
+
+define("_BOT_MANAGEMENT", "BOT MANAGEMENT");
+define("_SETTINGS", "Settings");
+define("_PLUGINS", "Plugins");
+
+define("_HELP", "HELP & SUPPORT");
+define("_VERSION_INFO", "Version Information");
