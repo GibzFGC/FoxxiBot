f603fb0c73626dd82611d7b46220673db67ca92c
Gibz <15359306+GibzFGC@users.noreply.github.com>
2022-05-02 03:41:42 +0100

    Added Application Title

--- source/Program.cs
+++ source/Program.cs
@@ -54,6 +54,9 @@ namespace FoxxiBot
 
         static void Main(string[] args)
         {
+            // Set Application Title
+            Console.Title = "FoxxiBot v1.0.2 Alpha - Revision 1 :: Started @ " + DateTime.Now.ToString("F");
+
             // Set Application Encoding
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
