6eaca2ccfad4319d8691f3809158069ecc5c695f
Gibz <15359306+GibzFGC@users.noreply.github.com>
2022-05-25 06:13:54 +0100

    Updated command listing for Discord

--- source/bin/Debug/net6.0/Web/pages/discord_commands/js/commands_add.js
+++ source/bin/Debug/net6.0/Web/pages/discord_commands/js/commands_add.js
@@ -3,7 +3,33 @@ function validityCheck(form) {
     // Check if name is a locked one
     var CommandName = form.commandName.value;
 
-    if (CommandName == "purge" || CommandName == "system" || CommandName == "status" || CommandName == "prefix" || CommandName == "promo" || CommandName == "say" || CommandName == "avatar") {
+    // List of Bot Controlled Commands
+    const bot_controlled = [
+        'avatar',
+        'anime',
+        'cat',
+        'cats',
+        'catfact',
+        'catfacts',
+        'dog',
+        'dogs',
+        'mal',
+        'meme',
+        'memes',
+        'prefix',
+        'promo',
+        'purge',
+        'prune',
+        'clear',
+        'ping',
+        'userinfo',
+        'status',
+        'system',
+        'tv'
+    ];
+
+    // Check if Command Name Reserved
+    if (bot_controlled.includes(CommandName)) {
         document.getElementById("commandNameError").innerHTML = "The Command Name given is a reserved one the bot uses <a id='commandNameErrorLink' href='#'><img width='12' height='12' src='main/templates/img/close.png' /></a>";
         
         document.getElementById('commandNameErrorLink').addEventListener('click',function(){
