a1f0598a67fcb088e45e21e667b6711e605b93d2
Anthony Gibson <antwanmedia@gmail.com>
2023-11-10 20:49:31 +0000

    File Reverted

--- source/TwitchBot/Twitch_Main.cs
+++ source/TwitchBot/Twitch_Main.cs
@@ -108,8 +108,8 @@ namespace FoxxiBot.TwitchBot
             pointsTimer = new Timer(pointsUpdate, null, 0, 300000);
 
             // Start Live Check Timer
-            //liveTimer = new Timer(streamLiveCallBack, null, 0, 10000);
-            liveTimer = new Timer(streamLiveCallBack, null, 0, 60000);
+            liveTimer = new Timer(streamLiveCallBack, null, 0, 10000);
+            //liveTimer = new Timer(streamLiveCallBack, null, 0, 60000);
 
             // Start OAuth Timer -- every 3 hours, 30 mins
             oauthTimer = new Timer(OauthCallback, null, 0, 12600000);
